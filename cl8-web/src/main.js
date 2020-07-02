22df3c3992bef1f8ad2b3e050748d3b80ab22a8e
rmparr <ryan@ryanmparr.com>
2020-07-02 18:08:50 +0200

    Update entry point.
    Move app out of a firebase callback.
    
    Remove firebase bits.
    
    Set user based on store.

--- cl8-web/src/main.js
+++ cl8-web/src/main.js
@@ -9,10 +9,7 @@ import router from './routes'
 import store from './store'
 
 import VeeValidate from 'vee-validate'
-import { rtdbPlugin } from 'vuefire'
 import VueFuse from 'vue-fuse'
-import fbase from './fbase'
-import VueAnalytics from 'vue-analytics'
 
 import debugLib from 'debug'
 const debug = debugLib('cl8.main.js')
@@ -37,28 +34,26 @@ const dict = {
 Vue.use(Vuex)
 Vue.use(VeeValidate, vvConfig)
 Vue.use(VueFuse)
-Vue.use(rtdbPlugin)
 
-Vue.use(VueAnalytics, {
-  id: process.env.VUE_APP_GOOGLE_ANALYTICS_UA,
-  router
-})
 const VueStore = new Vuex.Store(store)
 
+const app = new Vue({
+  router,
+  store: VueStore,
+  render: h => h(App)
+}).$mount('#app')
+
+app.$validator.localize('en', dict)
+
 router.beforeEach((to, from, next) => {
-  debug(to.name, to.from)
-  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-  let currentUser = fbase.auth().currentUser
-  debug('currentUser:', currentUser)
+  debug(to.name, to.from, next)
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const currentUser = VueStore.getters.profile
 
   if (currentUser && to.name === 'signin') {
     next('home')
   }
 
-  if (currentUser) {
-    debug(`store.commit('setFBUser', ${currentUser})`)
-    VueStore.commit('setFBUser', currentUser)
-  }
   if (requiresAuth && !currentUser) {
     // you need to be logged in, so log the user in
     debug(`signing in, as we have no fbase user`)
@@ -68,22 +63,16 @@ router.beforeEach((to, from, next) => {
   }
 })
 
-let app
-// wrapping the vue app here forces us to wait til we have the firebase object loaded
-// before we load Vue object
-fbase.auth().onAuthStateChanged(firebaseUser => {
-  /* eslint-disable no-new */
-  if (firebaseUser) {
-    debug('user:', firebaseUser.displayName)
-  }
-  if (!app) {
-    VueStore.commit('stopLoading')
-    app = new Vue({
-      router,
-      store: VueStore,
-      render: h => h(App)
-    }).$mount('#app')
+// // wrapping the vue app here forces us to wait til we have the firebase object loaded
+// // before we load Vue object
+// // fbase.auth().onAuthStateChanged(firebaseUser => {
+// /* eslint-disable no-new */
+// // if (firebaseUser) {
+// //   debug('user:', firebaseUser.displayName)
+// // }
+// if (!app) {
+//   VueStore.commit('stopLoading')
 
-    app.$validator.localize('en', dict)
-  }
-})
+// }
+
+// // })
