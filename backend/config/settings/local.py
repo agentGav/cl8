31e8d00d32c7a8c525edcfe6eda3e828b0876f6a
Chris Adams <chris@productscience.co.uk>
2020-07-10 11:28:52 +0200

    Add default local ports for Vue on C's machine

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -60,6 +60,8 @@ INSTALLED_APPS += ["django_extensions"]  # noqa F405
 # ------------------------------------------------------------------------------
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8081",
-    "http://127.0.0.1:8081"
+    "http://127.0.0.1:8081",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 CORS_ALLOW_CREDENTIALS = True
