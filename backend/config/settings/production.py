9a6fedfe21d6a8293bdae79cdf641ad88d0b58fa
Chris Adams <chris@productscience.co.uk>
2020-07-16 08:46:35 +0200

    Make whitenoise stop triggering 500s on missing favicons

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -87,6 +87,12 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
+
+# This stops whitenoise causing a 500 when there are missing files, like favicons
+# and so on
+# http://whitenoise.evans.io/en/stable/django.html?highlight=favicon#WHITENOISE_MANIFEST_STRICT
+WHITENOISE_MANIFEST_STRICT = False
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
