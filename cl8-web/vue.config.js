443a086c474b6d03981d87f9d50b634f31961e79
Chris Adams <chris@productscience.co.uk>
2020-07-21 06:03:35 +0200

    Update the dev handling so we proxy requests
    This saves us form hard coding domains, and seems to be
    the 'blessed' way in Vue

--- cl8-web/vue.config.js
+++ cl8-web/vue.config.js
@@ -1,7 +1,12 @@
 // vue.config.js
 module.exports = {
-  publicPath: './static/',
-  filenameHashing: false
+  // publicPath: './static/',
+  filenameHashing: false,
+  devServer: {
+    proxy: 'http://127.0.0.1:8000'
+  }
+
+
 }
 
 
