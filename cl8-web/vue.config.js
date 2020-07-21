71e4dc8de949b616447549590b18d4d83cc5330b
Chris Adams <chris@productscience.co.uk>
2020-07-21 10:38:40 +0200

    Update vue config for workign with django backend
    We use the proxy to send requests to django,
    and we create django compatible builds, putting the files where django expects them

--- cl8-web/vue.config.js
+++ cl8-web/vue.config.js
@@ -1,12 +1,19 @@
 // vue.config.js
 module.exports = {
-  // publicPath: './static/',
+  // serve this for `npm run serve` in development
+  // but output to the /static/ dir for django when
+  // deploying the final bundle
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/static/'
+    : '/',
+  outputDir: 'vue-static',
+  // django and whitenoise does its own hashing and cache busting
   filenameHashing: false,
+  // proxy requests that are not matched by our own files
+  // to the django server running at the address below
   devServer: {
     proxy: 'http://127.0.0.1:8000'
   }
-
-
 }
 
 
