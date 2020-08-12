52aad89de129e2e3a9ac80c14fcd7a408496e196
Chris Adams <chris@productscience.co.uk>
2020-08-12 20:09:16 +0200

    Update cancel/save styling

--- cl8-web/src/components/shared/NavHeaderEdit.vue
+++ cl8-web/src/components/shared/NavHeaderEdit.vue
@@ -2,16 +2,17 @@
   <div class="editprofile cf bg-white">
     <nav class="pa3 ph3 ph4-ns bb b--light-gray flex items-center">
       <span class="w-100">Edit profile</span>
+      
+      <router-link
+        :to="{ name: 'home' }"
+        class="fr v-btm f6 link br2 ph3 pv2 dib gray bg-white hover-bg-dark-gray hover-white"
+      >Cancel</router-link>
       <a
         href="#"
         v-on:submit.prevent="onSubmit"
         @click="onSubmit"
-        class="fr v-btm f6 mr3 link br2 ph3 pv2 dib white bg-gray hover-bg-green"
+        class="fr v-btm f6 ml3 link br2 ph3 pv2 dib white bg-gray hover-bg-green"
       >Save</a>
-      <router-link
-        :to="{ name: 'home' }"
-        class="fr v-btm f6 link br2 ph3 pv2 dib white bg-gray hover-bg-dark-gray"
-      >cancel</router-link>
     </nav>
   </div>
 </template>
