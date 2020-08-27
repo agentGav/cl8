4af0333bf443a04c9955bb766f6fc56df52122bd
Chris Adams <chris@productscience.co.uk>
2020-08-27 09:49:13 +0200

    Remove unused imports and reformat

--- backend/backend/users/api/passwordless_views.py
+++ backend/backend/users/api/passwordless_views.py
@@ -1,44 +1,18 @@
-from drfpasswordless.views import ObtainEmailCallbackToken
-from drfpasswordless.services import TokenService
-from drfpasswordless.utils import inject_template_context
-from drfpasswordless.settings import api_settings
-from rest_framework import parsers, renderers, status
-
 import logging
-from django.utils.module_loading import import_string
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.views import APIView
-from drfpasswordless.models import CallbackToken
-from drfpasswordless.settings import api_settings
-from drfpasswordless.serializers import (
-    EmailAuthSerializer,
-    MobileAuthSerializer,
-    CallbackTokenAuthSerializer,
-    CallbackTokenVerificationSerializer,
-    EmailVerificationSerializer,
-    MobileVerificationSerializer,
-)
-from django.contrib.auth import get_user_model
-from django.core.exceptions import PermissionDenied
+
 from django.core.mail import send_mail
 from django.template import loader
-from django.utils import timezone
-from rest_framework.authtoken.models import Token
-from drfpasswordless.models import CallbackToken
 from drfpasswordless.settings import api_settings
-
-
-
 from drfpasswordless.utils import (
     create_callback_token_for_user,
-    send_email_with_callback_token,
+    inject_template_context,
     send_sms_with_callback_token,
 )
+from drfpasswordless.views import ObtainEmailCallbackToken
+from rest_framework import status
+from rest_framework.response import Response
 
 logger = logging.getLogger(__name__)
-User = get_user_model()
-
 
 
 def send_email_with_callback_token(user, email_token, **kwargs):
@@ -52,18 +26,20 @@ def send_email_with_callback_token(user, email_token, **kwargs):
             # Make sure we have a sending address before sending.
 
             # Get email subject and message
-            email_subject = kwargs.get('email_subject',
-                                       api_settings.PASSWORDLESS_EMAIL_SUBJECT)
-            email_plaintext = kwargs.get('email_plaintext',
-                                         api_settings.PASSWORDLESS_EMAIL_PLAINTEXT_MESSAGE)
-            email_html = kwargs.get('email_html',
-                                    api_settings.PASSWORDLESS_EMAIL_TOKEN_HTML_TEMPLATE_NAME)
+            email_subject = kwargs.get(
+                "email_subject", api_settings.PASSWORDLESS_EMAIL_SUBJECT
+            )
+            email_plaintext = kwargs.get(
+                "email_plaintext", api_settings.PASSWORDLESS_EMAIL_PLAINTEXT_MESSAGE
+            )
+            email_html = kwargs.get(
+                "email_html", api_settings.PASSWORDLESS_EMAIL_TOKEN_HTML_TEMPLATE_NAME
+            )
 
             # Inject context if user specifies.
-            context = inject_template_context({
-                'callback_token': email_token.key,
-                'user': user,
-            })
+            context = inject_template_context(
+                {"callback_token": email_token.key, "user": user}
+            )
             html_message = loader.render_to_string(email_html, context,)
             send_mail(
                 email_subject,
@@ -71,20 +47,25 @@ def send_email_with_callback_token(user, email_token, **kwargs):
                 api_settings.PASSWORDLESS_EMAIL_NOREPLY_ADDRESS,
                 [getattr(user, api_settings.PASSWORDLESS_USER_EMAIL_FIELD_NAME)],
                 fail_silently=False,
-                html_message=html_message,)
+                html_message=html_message,
+            )
 
         else:
-            logger.debug("Failed to send token email. Missing PASSWORDLESS_EMAIL_NOREPLY_ADDRESS.")
+            logger.debug(
+                "Failed to send token email. Missing PASSWORDLESS_EMAIL_NOREPLY_ADDRESS."
+            )
             return False
         return True
 
-    except Exception as e:
-        logger.debug("Failed to send token email to user: %d."
-                  "Possibly no email on user object. Email entered was %s" %
-                  (user.id, getattr(user, api_settings.PASSWORDLESS_USER_EMAIL_FIELD_NAME)))
-        logger.debug(e)
-        return False
+    except Exception as e:  # NOQA: W0703
 
+        logger.debug(
+            "Failed to send token email to user: %d."
+            "Possibly no email on user object. Email entered was %s"
+            % (user.id, getattr(user, api_settings.PASSWORDLESS_USER_EMAIL_FIELD_NAME))
+        )
+        logger.error(e)
+        return False
 
 
 class RicherContextTokenService:
@@ -92,9 +73,9 @@ class RicherContextTokenService:
     def send_token(user, alias_type, token_type, **message_payload):
         token = create_callback_token_for_user(user, alias_type, token_type)
         send_action = None
-        if alias_type == 'email':
+        if alias_type == "email":
             send_action = send_email_with_callback_token
-        elif alias_type == 'mobile':
+        elif alias_type == "mobile":
             send_action = send_sms_with_callback_token
         # Send to alias
         success = send_action(user, token, **message_payload)
@@ -102,19 +83,21 @@ class RicherContextTokenService:
 
 
 class ConstellateEmailCallbackToken(ObtainEmailCallbackToken):
-
-
     def post(self, request, *args, **kwargs):
         if self.alias_type.upper() not in api_settings.PASSWORDLESS_AUTH_TYPES:
             # Only allow auth types allowed in settings.
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
         if serializer.is_valid(raise_exception=True):
             # Validate -
-            user = serializer.validated_data['user']
+            user = serializer.validated_data["user"]
             # Create and send callback token
-            success = RicherContextTokenService.send_token(user, self.alias_type, self.token_type, **self.message_payload)
+            success = RicherContextTokenService.send_token(
+                user, self.alias_type, self.token_type, **self.message_payload
+            )
 
             # Respond With Success Or Failure of Sent
             if success:
@@ -123,6 +106,8 @@ class ConstellateEmailCallbackToken(ObtainEmailCallbackToken):
             else:
                 status_code = status.HTTP_400_BAD_REQUEST
                 response_detail = self.failure_response
-            return Response({'detail': response_detail}, status=status_code)
+            return Response({"detail": response_detail}, status=status_code)
         else:
-            return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.error_messages, status=status.HTTP_400_BAD_REQUEST
+            )
