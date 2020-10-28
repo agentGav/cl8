5b5e6ebb61a9b1ce2dc53146e65d4f6dfa24d7b0
Chris Adams <chris@productscience.co.uk>
2020-10-28 19:24:07 +0100

    Swap in viewtify for the initial login

--- frontend/src/components/auth/Login.vue
+++ frontend/src/components/auth/Login.vue
@@ -1,147 +1,31 @@
 <template>
-  <div class="w-100">
-    <div v-if="loading">
-      <div class="spinner">
-        <img src="../../assets/loading.svg" alt="loading" />
-      </div>
-    </div>
+  <v-card width="400px" class="mt-5 mx-auto">
+    <v-card-title>
+      <h1 class="text-center text-h5">Icebreaker One Constellation</h1>
+    </v-card-title>
+    <v-card-text>
+      <p>
+        {{ $t('message.login.instructions') }}
+      </p>
+      <v-form>
+        <v-text-field label="your email address" />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn class="ml-2" color="info">Request login code</v-btn>
+    </v-card-actions>
+    <v-card-text>
+      <p class="text-caption">
+        <em>
+          {{ $t('message.login.helpInstructions.question') }}
 
-    <div v-if="!loading">
-      <div class="v-mid sign-in-prompt">
-        <h2 class="pt5 tc fw3 tracked">{{ $t('message.global.siteName') }}</h2>
-        <div class="w-100 tc">
-          <div role="status" aria-live="polite" class="vh dib">
-            <!--
-              when there is an error, we want list it in here, to a screen reader
-              can pick it up and read out the announcement
-            -->
-            <div v-if="errors" class="errors">
-              <p v-for="key in errors.all()" v-bind:key="key">{{ key }}</p>
-            </div>
-          </div>
-
-          <div
-            v-if="signInData.message"
-            role="status"
-            aria-live="polite"
-            class="flex items-center justify-center pa3 bg-lightest-blue navy f6"
-          >
-            <svg
-              class="w1"
-              data-icon="info"
-              viewBox="0 0 32 32"
-              style="fill: currentcolor"
-            >
-              <title>info icon</title>
-              <path
-                d="M16 0 A16 16 0 0 1 16 32 A16 16 0 0 1 16 0 M19 15 L13 15 L13 26 L19 26 z M16 6 A3 3 0 0 0 16 12 A3 3 0 0 0 16 6"
-              />
-            </svg>
-            <span class="ml3">{{ signInData.message }}</span>
-          </div>
-
-          <form v-on:submit.prevent class="w-100 pa3 dib border-box mw6 ph5">
-            <p class="gray measure tl lh-copy">
-              {{ $t('message.login.instructions') }}
-            </p>
-            <div class="w-100 mb3">
-              <input
-                type="text"
-                name="email"
-                v-model="email"
-                class="input-reset br2 pa2 ba b--light-gray mt1 w-100"
-                :class="{
-                  'bg-washed-red b--red': errors ? errors.has('email') : null
-                }"
-                :placeholder="$t('message.login.form.placeholder')"
-                :aria-label="$t('message.login.form.placeholder')"
-                autocomplete="email"
-                @input="checkForValidFormSubmission"
-                :disabled="emailSubmitted"
-              />
-
-              <div>
-                <small v-if="errors && errors.has('email')" class="red">{{
-                  errors.first('email')
-                }}</small>
-              </div>
-            </div>
-
-            <div v-if="emailSubmitted">
-              <div class="w-100">
-                <p class="gray measure tl lh-copy next-step-guidance">
-                  {{ $t('message.login.emailSubmitted.nextStepGuidance') }}
-                </p>
-                <input
-                  type="password"
-                  name="login-code"
-                  v-model="token"
-                  class="input-reset pa2 ba br2 b--light-gray w-100"
-                  :class="{
-                    'bg-washed-red b--red': errors && errors.has('token')
-                  }"
-                  :placeholder="$t('message.login.emailSubmitted.loginCode')"
-                  :aria-label="$t('message.login.emailSubmitted.loginCode')"
-                />
-
-                <div>
-                  <small v-if="errors && errors.has('token')" class="red">
-                    {{ errors.first('token') }}
-                  </small>
-                </div>
-              </div>
-              <div class="mt2 cf">
-                <button
-                  class="f6 link br3 bn pv2 mb2 mt2 bg-light-silver b white w-60 ml0 mr1 fl"
-                  :class="{
-                    'bg-green pointer grow hover-bg-dark-green': formValid
-                  }"
-                  :disabled="!formValid"
-                  name="sign-in"
-                  @click="signIn"
-                >
-                  {{ $t('message.login.emailSubmitted.signinButton') }}
-                </button>
-                <button
-                  class="f6 link br3 bn pv2 mb2 mt2 bg-light-silver b white w-60 ml0 mr1 fl bg-red pointer grow hover-bg-dark-red"
-                  name="button"
-                  @click="resetForm"
-                >
-                  {{ $t('message.login.emailSubmitted.resetButton') }}
-                </button>
-              </div>
-            </div>
-            <div v-else>
-              <div class="mt2 cf">
-                <button
-                  class="f6 link br3 bn pv2 mb2 mt2 bg-light-silver b white w-60 ml0 mr1 fl"
-                  :class="{
-                    'bg-green pointer grow hover-bg-dark-green': formValid
-                  }"
-                  :disabled="!formValid"
-                  name="button"
-                  @click="submitEmail"
-                >
-                  {{ $t('message.login.form.requestLoginCodeButton') }}
-                </button>
-              </div>
-            </div>
-          </form>
-
-          <footer>
-            <p class="f6 tc gray">
-              <em>
-                {{ $t('message.login.helpInstructions.question') }}
-                <a class="f6 tc gray" :href="supportEmail">
-                  {{ $t('message.login.helpInstructions.suggestion') }} </a
-                >.
-              </em>
-            </p>
-          </footer>
-        </div>
-      </div>
-    </div>
-  </div>
+          <a class="" :href="supportEmail">
+            {{ $t('message.login.helpInstructions.suggestion') }} </a
+          >.
+        </em>
+      </p>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
