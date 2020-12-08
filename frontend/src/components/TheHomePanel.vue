34feb9710ab912aaf458424f14a35ef6b7e2c1eb
Chris Adams <chris@productscience.co.uk>
2020-12-08 19:10:46 +0100

    Update the nav menus to use the vuetify nav
    This actually gets rid of a bunch of code, the
    keyboard navigation is actually pretty accessible.

--- frontend/src/components/TheHomePanel.vue
+++ frontend/src/components/TheHomePanel.vue
@@ -1,72 +1,62 @@
 <template>
   <div>
     <v-row>
-      
       <v-container fluid>
         <v-row>
           <v-col>
-            <v-card elevation=1>  
-              <v-card-actions>
-              </v-card-actions>
-              <v-form> 
+            <v-card elevation="1">
+              <v-card-actions> </v-card-actions>
+              <v-form>
                 <v-col class="py-0 my-0">
                   <!-- // placeholder="Search across tags, names and profile details" -->
                   <v-text-field
                     outlined
                     :placeholder="$t('message.navHeaderLoggedIn.search')"
                     name="search-term"
+                    clearable
                     @input="updateSearchTerm"
                   ></v-text-field>
                   <v-fade-transition>
-                  <div class="tag-list" v-if="activeTags.length > 0">
-                    <p>
-                      <span class="pr-2">Tags:</span>
-                      <v-chip
-                        color="primary"
-                        v-for="tag in activeTags"
-                        :key="tag"
-                        :data-name="tag"
-                        close
-                        @click.stop.prevent="toggleTag"
-                        >{{ tag }}
-                      </v-chip>
-                    </p>
-                  </div>
-                   </v-fade-transition>
-                  <v-fade-transition>
-
-                  <div class="tag-list" v-if="activeClusters.length > 0">
-                    <p>
-                      <span class="pr-2">Clusters:</span>
-                      <v-chip
-                        color="primary"
-                        v-for="cluster in activeClusters"
-                        :key="cluster"
-                        :data-name="cluster"
-                        @click.stop.prevent="toggleCluster"
-                        close
-                        >{{ cluster }}
-                      </v-chip>
-                    </p>
-                  </div>
+                    <div class="tag-list" v-if="activeTags.length > 0">
+                      <p>
+                        <span class="pr-2">Tags:</span>
+                        <v-chip
+                          color="primary"
+                          v-for="tag in activeTags"
+                          :key="tag"
+                          :data-name="tag"
+                          close
+                          @click.stop.prevent="toggleTag"
+                          >{{ tag }}
+                        </v-chip>
+                      </p>
+                    </div>
                   </v-fade-transition>
-                  
+                  <v-fade-transition>
+                    <div class="tag-list" v-if="activeClusters.length > 0">
+                      <p>
+                        <span class="pr-2">Clusters:</span>
+                        <v-chip
+                          color="primary"
+                          v-for="cluster in activeClusters"
+                          :key="cluster"
+                          :data-name="cluster"
+                          @click.stop.prevent="toggleCluster"
+                          close
+                          >{{ cluster }}
+                        </v-chip>
+                      </p>
+                    </div>
+                  </v-fade-transition>
+
                   <v-card-actions>
-                    <v-btn
-                      color="red"
-                      text
-                      small
-                    >
-                      Clear
-                    </v-btn>
-    </v-card-actions>
+                    <v-btn color="red" text small> Clear </v-btn>
+                  </v-card-actions>
                 </v-col>
-                      
               </v-form>
             </v-card>
           </v-col>
         </v-row>
-        
       </v-container>
     </v-row>
     <v-row>
@@ -74,11 +64,10 @@
         <profile-detail />
       </v-col>
       <v-col cols="12" sm="4" :class="profileListClassObject">
-        <v-card elevation=1>
-          
-        <div class="scrollable-height">
-          <the-profile-list />
-        </div>
+        <v-card elevation="1">
+          <div class="scrollable-height">
+            <the-profile-list />
+          </div>
         </v-card>
       </v-col>
     </v-row>
@@ -88,111 +77,110 @@
 
 <script>
 /* eslint-disable */
-import ProfileDetail from '@/components/profile/ProfileDetail.vue'
-import ProfileSearchItem from '@/components/profile/ProfileSearchItem.vue'
-import TheProfileList from '@/components/TheProfileList.vue'
-import TheFooter from '@/components/TheFooter.vue'
+import ProfileDetail from "@/components/profile/ProfileDetail.vue";
+import ProfileSearchItem from "@/components/profile/ProfileSearchItem.vue";
+import TheProfileList from "@/components/TheProfileList.vue";
+import TheFooter from "@/components/TheFooter.vue";
 
-import store from '@/store'
-import debugLib from 'debug'
-const debug = debugLib('cl8.TheHomePanel')
+import store from "@/store";
+import debugLib from "debug";
+const debug = debugLib("cl8.TheHomePanel");
 
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
-const VueStore = new Vuex.Store(store)
+Vue.use(Vuex);
+const VueStore = new Vuex.Store(store);
 
 export default {
-  name: 'TheHomePanel',
+  name: "TheHomePanel",
   components: {
     ProfileDetail,
     TheProfileList,
-    TheFooter
+    TheFooter,
   },
 
   data() {
     return {
-      showingProfile: false
-    }
+      showingProfile: false,
+    };
   },
   computed: {
     profile() {
-      return this.$store.getters.profile
+      return this.$store.getters.profile;
     },
     profileDetailClassObject: function () {
-      // if there is no profile, 
+      // if there is no profile,
       // screens larger than a phone
       return {
-        'd-sm-block': this.profile,
-        'd-none d-sm-block': !this.showingProfile
-      }
+        "d-sm-block": this.profile,
+        "d-none d-sm-block": !this.showingProfile,
+      };
     },
     profileListClassObject: function () {
-      // only show the full profile list on 
+      // only show the full profile list on
       // screens larger than a phone
       return {
-        'd-block': !this.profile,
-        'd-none d-sm-block': this.profile
-      }
+        "d-block": !this.profile,
+        "d-none d-sm-block": this.profile,
+      };
     },
     term() {
-      return this.$store.getters.currentTerm
+      return this.$store.getters.currentTerm;
     },
     activeTags() {
-      return this.$store.getters.activeTags
+      return this.$store.getters.activeTags;
     },
     activeClusters() {
-      return this.$store.getters.activeClusters
-    }
+      return this.$store.getters.activeClusters;
+    },
   },
   watch: {},
   methods: {
     toggleTag: function (ev) {
-      const tag = ev.target.textContent.trim()
-      debug('toggleTag', { tag })
-      this.$store.dispatch('updateActiveTags', tag)
+      const tag = ev.target.textContent.trim();
+      debug("toggleTag", { tag });
+      this.$store.dispatch("updateActiveTags", tag);
     },
     toggleCluster: function (ev) {
-      const cluster = ev.target.textContent.trim()
-      debug('toggleCluster', { cluster })
-      this.$store.dispatch('updateActiveClusters', cluster)
+      const cluster = ev.target.textContent.trim();
+      debug("toggleCluster", { cluster });
+      this.$store.dispatch("updateActiveClusters", cluster);
     },
     updateSearchTerm(term) {
-      this.$store.commit('setTerm', term)
-    }
+      this.$store.commit("setTerm", term);
+    },
   },
   created() {
     if (this.$route.params.profileId) {
-      this.showingProfile = true
-      debug({showingProfile: this.showingProfile})
+      this.showingProfile = true;
+      debug({ showingProfile: this.showingProfile });
     }
   },
-  async beforeRouteEnter(routeTo, routeFrom, next)  {
-        debug("beforeRouteEnter")
-        if (routeTo.params.profileId) {
-          await VueStore.dispatch('fetchProfile', {id: routeTo.params.profileId})
-        }
-        next()
+  async beforeRouteEnter(routeTo, routeFrom, next) {
+    debug("beforeRouteEnter");
+    if (routeTo.params.profileId) {
+      await VueStore.dispatch("fetchProfile", { id: routeTo.params.profileId });
+    }
+    next();
   },
-  async beforeRouteUpdate(routeTo, routeFrom, next)  {
-        debug("beforeRouteUpdate")
-        if (routeTo.params.profileId) {
-          this.showingProfile = true
-          debug({showingProfile: this.showingProfile})
-          await this.$store.dispatch('fetchProfile', {id: routeTo.params.profileId})
-        }
-        next()
+  async beforeRouteUpdate(routeTo, routeFrom, next) {
+    debug("beforeRouteUpdate");
+    if (routeTo.params.profileId) {
+      this.showingProfile = true;
+      debug({ showingProfile: this.showingProfile });
+      await this.$store.dispatch("fetchProfile", { id: routeTo.params.profileId });
+    }
+    next();
   },
-}
+};
 </script>
 
 <style>
-
 .scrollable-height {
-  @media (min-width: 600px){
-    max-height:77vh;
-    overflow:auto;
+  @media (min-width: 600px) {
+    max-height: 77vh;
+    overflow: auto;
   }
 }
 </style>
--- frontend/src/components/shared/NavHeaderLoggedIn.vue
+++ frontend/src/components/shared/NavHeaderLoggedIn.vue
@@ -2,43 +2,22 @@
   <div v-if="loggedIn">
     <v-menu offset-y>
       <template v-slot:activator="{ on, attrs }">
-        <v-btn color="primary"  v-bind="attrs" v-on="on">
+        <v-btn color="primary" v-bind="attrs" v-on="on">
           <v-icon aria-label="Navigation" role="img" aria-hidden="false">
             {{ icons.mdiMenu }}
           </v-icon>
         </v-btn>
       </template>
       <v-list>
-        <v-list-item>
-          <router-link
-            v-if="canAddUsers"
-            :to="{ name: 'addUser' }"
-            role="link"
-            tabindex="0"
-            class="link dark-gray f6 nowrap f6-ns dib fr fn-ns pointer pt0 pb3 pa2-m pa2-l ph3 v-mid order-1 tr"
-            >{{ $t('message.shared.addUser') }}</router-link
-          >
+        <v-list-item @click="addUser" v-if="canAddUsers">
+          {{ $t("message.shared.addUser") }}
         </v-list-item>
 
-        <v-list-item>
-          <span
-            :title="$t('message.navHeaderLoggedIn.myProfile')"
-            role="link"
-            tabindex="0"
-            class="link dark-gray f6 nowrap f6-ns dib fr fn-ns pointer pt0 pb3 pa2-m pa2-l ph3 v-mid order-1 tr"
-            @click="myProfile"
-            >{{ $t('message.navHeaderLoggedIn.myProfile') }}</span
-          >
+        <v-list-item @click="myProfile">
+          {{ $t("message.navHeaderLoggedIn.myProfile") }}
         </v-list-item>
-        <v-list-item>
-          <span
-            :title="$t('message.navHeaderLoggedIn.logOut')"
-            role="link"
-            tabindex="0"
-            class="link dark-gray f6 nowrap f6-ns dib fr fn-ns pointer pt0 pb3 pa2-m pa2-l ph3 v-mid order-1 tr"
-            @click="logout"
-            >{{ $t('message.navHeaderLoggedIn.logOut') }}</span
-          >
+        <v-list-item @click="logout">
+          {{ $t("message.navHeaderLoggedIn.logOut") }}
         </v-list-item>
       </v-list>
     </v-menu>
@@ -46,40 +25,43 @@
 </template>
 
 <script>
-import debugLib from 'debug'
-import { mdiMenu } from '@mdi/js'
+import debugLib from "debug";
+import { mdiMenu } from "@mdi/js";
 
-const debug = debugLib('cl8.NavHeaderLoggedIn.vue')
+const debug = debugLib("cl8.NavHeaderLoggedIn.vue");
 export default {
-  name: 'Header',
+  name: "Header",
   data() {
     return {
       icons: {
         mdiMenu,
-      }
-    }
+      },
+    };
   },
   computed: {
     canAddUsers() {
-      return this.$store.getters.isAdmin
+      return this.$store.getters.isAdmin;
     },
     loggedIn() {
-      return this.$store.getters.currentUser
-    }
+      return this.$store.getters.currentUser;
+    },
   },
   methods: {
+    addUser() {
+      this.$router.push({ name: "addUser" });
+    },
     logout: function () {
-      debug('log out')
-      this.$store.dispatch('logout')
+      debug("log out");
+      this.$store.dispatch("logout");
     },
     myProfile: function () {
-      debug('setting profile back to user')
-      const currentUserId = this.$store.getters.currentUser.id
+      debug("setting profile back to user");
+      const currentUserId = this.$store.getters.currentUser.id;
       this.$router.push({
-        name: 'viewProfile',
-        params: { profileId: currentUserId }
-      })
-    }
-  }
-}
+        name: "viewProfile",
+        params: { profileId: currentUserId },
+      });
+    },
+  },
+};
 </script>
