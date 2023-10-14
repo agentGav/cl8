6156ee2600a315a81a803332665b45aae66f72a1
Chris Adams <chris@productscience.co.uk>
2023-10-14 13:21:36 +0200

    Add tracing back with sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -178,15 +178,22 @@ LOGGING = {
 # Sentry
 # ------------------------------------------------------------------------------
 
-# SENTRY_DSN = env("SENTRY_DSN")
+SENTRY_DSN = env("SENTRY_DSN")
 
-# SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
+SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
 
-# sentry_logging = LoggingIntegration(
-#     level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
-#     event_level=logging.ERROR,  # Send errors as events
-# )
-# sentry_sdk.init(dsn=SENTRY_DSN, integrations=[sentry_logging, DjangoIntegration()])
+sentry_logging = LoggingIntegration(
+    level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
+    event_level=logging.ERROR,  # Send errors as events
+
+
+)
+sentry_sdk.init(
+    dsn=SENTRY_DSN, 
+    # add tracing to see slow loading
+    traces_sample_rate=float(1.0),
+    # list error logs as exception events for sentry
+    integrations=[sentry_logging, DjangoIntegration()])
 
 # Your stuff...
 # ------------------------------------------------------------------------------
