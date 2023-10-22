d5202ba72e28a852a15da625380047770260e4a6
Chris Adams <chris@productscience.co.uk>
2023-10-22 13:24:35 +0200

    Comment out the select2 autocomplete
    Went we have the autocomplete activate, clicking
    tags does not work .

--- cl8/users/filters.py
+++ cl8/users/filters.py
@@ -15,8 +15,11 @@ class ProfileFilter(django_filters.FilterSet):
         field_name="tags",
         label="Tags",
         queryset=taggit_models.Tag.objects.all(),
-        widget=autocomplete.ModelSelect2Multiple(url="tag-autocomplete"),
-        conjoined=True,
+        # comment these out, and remove the hidden class
+        # on the profile filter template to use the select2 autocomplete
+        # widget
+        # widget=autocomplete.ModelSelect2Multiple(url="tag-autocomplete"),
+        # conjoined=True,
     )
 
     def search_fulltext(self, queryset, field_name, value):
