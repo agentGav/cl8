c2d9a2adeb775746a79db859fcf1c0e8fd6ce969
Chris Adams <chris@productscience.co.uk>
2024-11-26 23:17:03 +0100

    Remove Chris from default admin notifications for errors

--- config/settings/base.py
+++ config/settings/base.py
@@ -279,7 +279,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Chris Adams""", "chris@productscience.net")]
+ADMINS = []
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
